043c2cc97d6fb567d38af260b76bffb7b1e829d5
Larrygf02 <raulgf_02@hotmail.com>
2022-02-20 19:45:40 -0500

    Collections

--- lambdas-and-collections/collection_funcs.py
+++ lambdas-and-collections/collection_funcs.py
@@ -1,5 +1,24 @@
+from functools import reduce
+
 domain = [1,2,3,4,5]
 
 #map is a high order function, this means a function that takes a function as an argument
 our_range = map(lambda num: num*2, domain)
 print(list(our_range))
+
+evens = filter(lambda num: num % 2 == 0, domain)
+print(list(evens))
+
+the_sum = reduce(lambda acc, num: acc + num, domain, 0)
+print(the_sum)
+
+words = ['Boss', 'a', 'Alfred', 'fig', 'Daemon', 'dig']
+print("Sorting by default")
+print(sorted(words))
+
+print("Sorting with a lambda key")
+print(sorted(words, key=lambda s:s.lower()))
+
+print("Sorting with a method")
+words.sort(key=str.lower, reverse=True)
+print(words)
