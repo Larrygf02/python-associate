b4647eb54ebbdf83fb69d518dd4804551d98197b
Larrygf02 <raulgf_02@hotmail.com>
2022-02-28 23:41:39 -0500

    hidden module entities

--- modules-packages/helper.py
+++ modules-packages/helper.py
@@ -1,8 +1,12 @@
-def extract_upper(phrase):
+#__all__ = ['extract_upper'] #It's means that only extract_upper function is imported when you call the module
+
+# If name function starts with underscorde '_' this function is not accesible
+def _extract_upper(phrase):
     return list(filter(str.isupper, phrase))
 
 def extract_lower(phrase):
     return list(filter(str.islower, phrase))
-print("something in helper")
+
+
 if __name__ == "__main__":
     print("Hello from helpers")
--- modules-packages/main.py
+++ modules-packages/main.py
@@ -1,6 +1,6 @@
-from helper import extract_lower, extract_upper
+from helper import *
 import extras
 
 #name = "keith Thompson"
 print(f"Lowercase Letters: {extract_lower(extras.name)}")
-print(f"Uppercase Letters: {extract_upper(extras.name)}")
+print(f"Uppercase Letters: {_extract_upper(extras.name)}")
