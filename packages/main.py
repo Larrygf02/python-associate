5436e5bc0692d73b6283e4c2e950e8fe6ef5ff06
Larrygf02 <raulgf_02@hotmail.com>
2022-03-12 10:31:59 -0500

    Creating package and init file

---
+++ packages/main.py
@@ -0,0 +1,4 @@
+from helper import extract_lower, extract_upper, name
+#name = "keith Thompson"
+print(f"Lowercase Letters: {extract_lower(name)}")
+print(f"Uppercase Letters: {extract_upper(name)}")
