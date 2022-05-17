560f5daa0770a6a686b64a8bf681e8a1c47b1430
Larrygf02 <raulgf_02@hotmail.com>
2022-05-17 00:31:05 -0500

    custom constructor

--- poo/vehicle.py
+++ poo/vehicle.py
@@ -1,9 +1,16 @@
 class Vehicle():
-    
+
+    default_tire = 'tire'
     def __init__(self, engine, tires):
         self.engine = engine
         self.tires = tires
         pass
+
+    @classmethod #this is a simple way that you can create custom constructor
+    def bicycle(cls, tires=None):
+        if not tires:
+            tires = [cls.default_tire, cls.default_tire]
+        return cls(None, tires)
 
     def description(self):
         print(f"a vehicle with an {self.engine} engine, and {self.tires} tires")
