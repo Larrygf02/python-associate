2fd6ac9c8afe25def60e38ed734de4ced81d8755
Larrygf02 <raulgf_02@hotmail.com>
2022-05-17 20:52:39 -0500

    Inherintance and super

--- poo/vehicle.py
+++ poo/vehicle.py
@@ -1,16 +1,17 @@
 class Vehicle():
 
-    default_tire = 'tire'
-    def __init__(self, engine, tires):
-        self.engine = engine
-        self.tires = tires
+    #default_tire = 'tire'
+    def __init__(self, distance_traveled=0, unit='miles'):
+        self.distance_traveled = distance_traveled
+        self.unit = unit
         pass
 
-    @classmethod #this is a simple way that you can create custom constructor
-    def bicycle(cls, tires=None):
-        if not tires:
-            tires = [cls.default_tire, cls.default_tire]
-        return cls(None, tires)
+    #@classmethod #this is a simple way that you can create custom constructor
+    #def bicycle(cls, tires=None):
+    #    if not tires:
+    #        tires = [cls.default_tire, cls.default_tire]
+    #    return cls(None, tires)
 
     def description(self):
-        print(f"a vehicle with an {self.engine} engine, and {self.tires} tires")
+        message = f"A {self.__class__.__name__} vehicle that has traveled {self.distance_traveled} {self.unit}"
+        return message
