84feda8662343ceaec6cc6a48d1caa08a6fd9404
Larrygf02 <raulgf_02@hotmail.com>
2022-05-20 20:34:06 -0500

    handling errno

---
+++ handling_files/using_error.py
@@ -0,0 +1,9 @@
+import errno
+
+try:
+    f = open('fake.txt', 'r')
+except OSError as err:
+    if err.errno == errno.ENOENT:
+        print("File not found")
+    elif err.errno == errno.EACCES:
+        print("Bad permissions")
